9cd021e2294c1ea90247bc970c3ef2e62d00df62
Kingsbug <810873968@qq.com>
2020-09-03 15:14:16 +0800

    "sss"

---
+++ src/components/BBB.vue
@@ -0,0 +1,20 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  methods: {},
+  // 生命周期 - 创建完成
+  created() {},
+
+  // DOM挂载完毕
+  mounted() {},
+};
+</script>
+
+<style scoped lang='scss'>
+</style>
